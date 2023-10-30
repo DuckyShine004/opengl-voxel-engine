fa54b31ac5cfa6dbbc05be32c8c575afe838db0b
Gallon Zhou <123701457+DuckyShine004@users.noreply.github.com>
2023-10-30 22:07:59 +1300

    refactor: Delete Shaders After Linking With Program

--- src/main.py
+++ src/main.py
@@ -1,10 +1,9 @@
 from __future__ import annotations
 
 from OpenGL.GL import *
+from glfw import *
 
 from manager.shader_manager import ShaderManager
-
-import glfw
 
 class App:
 	def __init__(self):
@@ -19,22 +18,22 @@ class App:
 		"""The main driver code."""
 
 		# Initialize glfw
-		if not glfw.init():
+		if not init():
 			print("glfw failed to initialize!")
 			exit(1)
 
 		# Setup OpenGL hints and create the window
-		glfw.window_hint(glfw.CONTEXT_VERSION_MINOR, 4)
-		glfw.window_hint(glfw.CONTEXT_VERSION_MAJOR, 4)
-		glfw.window_hint(glfw.OPENGL_PROFILE, glfw.OPENGL_CORE_PROFILE)
+		window_hint(CONTEXT_VERSION_MINOR, 4)
+		window_hint(CONTEXT_VERSION_MAJOR, 4)
+		window_hint(OPENGL_PROFILE, OPENGL_CORE_PROFILE)
 
-		self.__window = glfw.create_window(800, 800, "window", None, None)
+		self.__window = create_window(800, 800, "window", None, None)
 
 		# Make the current context the application window 
-		glfw.make_context_current(self.__window)
+		make_context_current(self.__window)
 
 		# Enable v-sync
-		glfw.swap_interval(1)
+		swap_interval(1)
 
 		# Generate the vertex array object
 		vao = GLuint()
@@ -43,8 +42,8 @@ class App:
 		glBindVertexArray(vao)
 
 	def __process_inputs(self) -> None:
-		if glfw.get_key(self.__window, glfw.KEY_ESCAPE) == glfw.PRESS:
-			glfw.set_window_should_close(self.__window, True)
+		if get_key(self.__window, KEY_ESCAPE) == PRESS:
+			set_window_should_close(self.__window, True)
 
 	def __display(self) -> None:
 		"""Updates the display on every frame."""
@@ -58,15 +57,16 @@ class App:
 		self.__shader_manager.use_shader_program()
 
 		# Update the window while it is not closed
-		while not glfw.window_should_close(self.__window):
-			self.__display()
+		while not window_should_close(self.__window):
 			self.__process_inputs()
 
-			glfw.swap_buffers(self.__window)
-			glfw.poll_events()
+			self.__display()
 
-		glfw.destroy_window(self.__window)
-		glfw.terminate()
+			swap_buffers(self.__window)
+			poll_events()
+
+		destroy_window(self.__window)
+		terminate()
 		exit(0)
 
 if __name__ == '__main__':
