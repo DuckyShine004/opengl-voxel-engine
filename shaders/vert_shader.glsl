f127627c638c8421d5e4d4151e7f5b09f5f3e428
Gallon Zhou <123701457+DuckyShine004@users.noreply.github.com>
2023-11-05 18:46:13 +1300

    bug: Removed Redundant Color Vertex Buffer

--- shaders/vert_shader.glsl
+++ shaders/vert_shader.glsl
@@ -1,10 +1,8 @@
 #version 430 core
 
 layout (location = 0) in vec3 aPos;
-layout (location = 1) in vec3 aColor;
-layout (location = 2) in vec3 aOffset;
+layout (location = 1) in vec3 aOffset;
 
-out vec3 ourColor;
 out vec3 fragPos;
 
 uniform mat4 view_matrix;
@@ -12,6 +10,5 @@ uniform mat4 projection_matrix;
 
 void main() {
 	gl_Position = projection_matrix * view_matrix * vec4(aPos + aOffset, 1.0f);
-	ourColor = aColor;
 	fragPos = aPos;
 }
