c6faf102618e70971e787140435bfc4de7f780e1
Gallon Zhou <123701457+DuckyShine004@users.noreply.github.com>
2023-11-08 00:20:38 +1300

    refactor: Changed The Texture Manager To Work With Texture Atlases And UVs

--- src/manager/texture_manager.py
+++ src/manager/texture_manager.py
@@ -7,41 +7,46 @@ from OpenGL.GL import *
 from PIL import Image
 
 from constants.file_constants import TEXTURE_LOCATION
+from constants.shape_constants import TEXTURE_WIDTH, TEXTURE_HEIGHT
 
 class TextureManager:
+    # @staticmethod
+    # def get_texture(texture_type: str) -> GLuint:
+    #     filenames = TextureManager.get_filenames(texture_type)
+
+    #     texture = glGenTextures(1)
+    #     glBindTexture(GL_TEXTURE_CUBE_MAP, texture)
+
+    #     for i, filename in enumerate(filenames):
+    #         image = Image.open(filename)
+    #         image_data = numpy.array(list(image.getdata()), dtype="uint8")
+
+    #         glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL_RGB, TEXTURE_WIDTH, TEXTURE_HEIGHT, 0, GL_RGBA, GL_UNSIGNED_BYTE, image_data)
+
+    #     # For pixelated effect we use the nearest filtering option
+    #     glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
+    #     glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+    #     glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE)
+    #     glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE)
+    #     glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE)
+
+    #     # Generate the texture mipmaps
+    #     glGenerateMipmap(GL_TEXTURE_CUBE_MAP)
+
+    #     return texture
+
     @staticmethod
-    def bind_texture(texture_type: str) -> GLuint:
-        filenames = TextureManager.get_filenames(texture_type)
+    def get_texture(texture_type: str) -> numpy.ndarray:
+        filename = TextureManager.get_filename(texture_type)
+        image = Image.open(filename)
+        texture_data = numpy.array(image.convert("RGBA"), dtype = "uint8")
 
-        texture = glGenTextures(1)
-        glBindTexture(GL_TEXTURE_CUBE_MAP, texture)
-
-        for i, filename in enumerate(filenames):
-            image = Image.open(filename)
-            image_data = numpy.array(list(image.getdata()), dtype="uint8")
-
-            width, height = image.size[0], image.size[1]
-
-            glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image_data)
-
-        # For pixelated effect we use the nearest filtering option
-        glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
-        glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
-        glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE)
-        glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE)
-        glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE)
-
-        # Generate the texture mipmaps
-        glGenerateMipmap(GL_TEXTURE_CUBE_MAP)
+        return texture_data
 
     @staticmethod
-    def get_filenames(texture_type: str) -> List[str]:
+    def get_filename(texture_type: str) -> List[str]:
         texture_location = TEXTURE_LOCATION + texture_type + '/'
-        block_type = texture_location + texture_type + "_block_"
+        filename = texture_location + texture_type + "_atlas.png"
 
-        top = block_type + "top.png"
-        side = block_type + "side.png"
-        bottom = block_type + "bottom.png"
-
-        return [side, side, top, bottom, side, side] 
+        return filename
 
