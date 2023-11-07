59ab0b442099f0496c71eeb608962cf9d5f1162e
Gallon Zhou <123701457+DuckyShine004@users.noreply.github.com>
2023-11-08 00:07:05 +1300

    refactor: Changed Shader Code To Work With Texture Arrays And UVs

--- shaders/vert_shader.glsl
+++ shaders/vert_shader.glsl
@@ -2,15 +2,19 @@
 
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aOffset;
+layout (location = 2) in float aTexIndex;
+layout (location = 3) in vec2 aTexCoord;
 
-out vec3 fragPos;
 out vec3 offsetPos;
+out float fragTexIndex;
+out vec2 fragTexCoord;
 
 uniform mat4 view;
 uniform mat4 projection;
 
 void main() {
 	gl_Position = projection * view * vec4(aPos + aOffset, 1.0f);
-	fragPos = aPos;
 	offsetPos = aPos + aOffset;
+	fragTexIndex = aTexIndex;
+	fragTexCoord = aTexCoord;
 }
