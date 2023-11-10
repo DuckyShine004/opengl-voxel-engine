084a567d7cecb54d9dd917bfc64fb52302b14eab
Gallon Zhou <123701457+DuckyShine004@users.noreply.github.com>
2023-11-10 21:36:05 +1300

    bug: Fixed Normals Calculation Bug For Flat Shading

--- shaders/frag_shader.glsl
+++ shaders/frag_shader.glsl
@@ -16,11 +16,15 @@ struct Light {
     vec3 specular;
 };
 
-in vec3 fragPos;
-in vec3 offsetPos;
-in float fragTexIndex;
-in vec2 fragTexCoord;
-in vec3 Normal;
+in GS_OUT {
+    vec3 fragPos;
+    vec3 offsetPos;
+
+    float fragTexIndex;
+
+    vec2 fragTexCoord;
+    vec3 normal;
+} fs_in;
 
 out vec4 FragColor;
 
@@ -40,32 +44,37 @@ void main() {
     vec3 ambient = light.ambient * material.ambient;
 
     // Diffuse
-    vec3 norm = normalize(Normal);
+    vec3 norm = normalize(fs_in.normal);
 
     // Point light
-    vec3 lightDir = normalize(light.position - offsetPos);
+    vec3 lightDir = normalize(light.position - fs_in.offsetPos);
 
     // Directional light
-    // vec3 lightDir = normalize(-light.direction);
+    //vec3 lightDir = normalize(-light.direction);
 
     float diff = max(dot(norm, lightDir), 0.0);
     vec3 diffuse = light.diffuse * (diff * material.diffuse);
 
     // Specular
-    vec3 viewDir = normalize(cameraPos - offsetPos);
+    vec3 viewDir = normalize(cameraPos - fs_in.offsetPos);
     vec3 reflectDir = reflect(-lightDir, norm); 
     float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shine);
     vec3 specular = light.specular * (spec * material.specular); 
 
     vec3 phong = ambient + diffuse + specular;
 
-    float distance = length(offsetPos - cameraPos);
+    // Fog
+    float distance = length(fs_in.offsetPos - cameraPos);
     float fogFactor = clamp((fogEnd - distance) / (fogEnd - fogStart), 0.0, 1.0);
 
-    vec4 texColor = texture(ourTextureArray, vec3(fragTexCoord, fragTexIndex));
+    // Texture sampling
+    vec4 texColor = texture(ourTextureArray, vec3(fs_in.fragTexCoord, fs_in.fragTexIndex));
     vec3 color = phong * texColor.rgb;
 
     color = mix(fogColor.rgb, color, fogFactor);
 
     FragColor = vec4(color, 1.0);
+
+    // Debugging
+    //FragColor = vec4((fs_in.normal + 1.0) / 2.0, 1.0);
 }
