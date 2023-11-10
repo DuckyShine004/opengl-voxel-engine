e1c663c488125d824ef0a20c6dcbc4694d970125
Gallon Zhou <123701457+DuckyShine004@users.noreply.github.com>
2023-11-10 22:41:28 +1300

    docs: Added Python Documentation For Utility Class

--- src/utility/utility.py
+++ src/utility/utility.py
@@ -1,11 +1,27 @@
+"""This module provides a way to access useful utilities."""
+
 from __future__ import annotations
 
 import numpy
 
 from numba import njit
 
+
 class Utility:
+
+    """Provides an interface to call useful methods."""
+
     @staticmethod
     def clamp(value: float, minimum: float, maximum: float) -> float:
-        return max(min(value, maximum), minimum)
+        """Clamps the given value between two ranges: minimum and maximum.
 
+        Args:
+            value (float): The value to be clamped.
+            minimum (float): The lower bound.
+            maximum (float): The upper bound.
+
+        Returns:
+            float: The clamped value.
+        """
+
+        return max(min(value, maximum), minimum)
