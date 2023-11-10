945de13ffcf3524a4ad51946684acd0f50b4975a
Gallon Zhou <123701457+DuckyShine004@users.noreply.github.com>
2023-11-10 16:50:15 +1300

    docs: Added Python Docs For Noise Module

--- src/world/noise.py
+++ src/world/noise.py
@@ -1,3 +1,4 @@
+"""This module initializes and optimizes the opensimplex noise module."""
 from __future__ import annotations
 
 import sys
@@ -9,10 +10,35 @@ from opensimplex.internals import _noise2, _noise3, _init
 
 perm, perm_grad_index3 = _init(seed=random.randint(0, sys.maxsize))
 
+
 @njit(cache=True)
-def get_noise_2d(x: float, y: float):
-	return _noise2(x, y, perm)
+def get_noise_2d(x: float, y: float) -> float:
+    """Return the 2d noise value. Most likely used to generate a height map
+    value.
+
+    Args:
+        x (float): The x coordinate.
+        y (float): The y coordinate.
+
+    Returns:
+        float: The 2d noise value.
+    """
+
+    return _noise2(x, y, perm)
+
 
 @njit(cache=True)
 def get_noise_3d(x: float, y: float, z: float) -> float:
-	return _noise3(x, y, z, perm, perm_grad_index3)
+    """Return the 3d noise value. Most likely used to compute which voxels are
+    needed in the context of cave generation.
+
+    Args:
+        x (float): The x coordinate.
+        y (float): The y coordinate.
+        z (float): The z coordinate.
+
+    Returns:
+        float: The 3d noise value.
+    """
+
+    return _noise3(x, y, z, perm, perm_grad_index3)
