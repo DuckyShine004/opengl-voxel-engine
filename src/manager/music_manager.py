e44381976ca520c88956f65c1cababf22f182ccc
Gallon Zhou <123701457+DuckyShine004@users.noreply.github.com>
2023-11-10 22:29:57 +1300

    docs: Added Python Documentation For Music Manager

--- src/manager/music_manager.py
+++ src/manager/music_manager.py
@@ -1,3 +1,5 @@
+"""The music manager manages anything related to music."""
+
 from __future__ import annotations
 
 import os
@@ -6,23 +8,19 @@ import pygame
 
 from constants.file_constants import MUSIC_LOCATION
 
-class MusicManager:
-    """The MusicManager class manages events related to music.
 
-    Attributes:
-    __music_path - a private variable which stores the path to our
-    source of music.
-    __music_list - a private variable which stores the list of all
-    music in the music directory.
-    __music_poll - a private variable which stores the number of
-    music in the music directory.
-    """
+class MusicManager:
+    """The MusicManager class manages events related to music."""
 
     def __init__(self) -> None:
+        """Initializes the music manager class."""
+
         self.__music_list = os.listdir(MUSIC_LOCATION)
         self.__music_poll = len(self.__music_list) - 1
 
     def play_music(self) -> None:
+        """Play the next music."""
+
         if not pygame.mixer.music.get_busy():
             music_rand = random.randint(0, self.__music_poll)
             music_path = MUSIC_LOCATION + self.__music_list[music_rand]
@@ -30,4 +28,4 @@ class MusicManager:
             pygame.mixer.music.load(music_path)
             pygame.mixer.music.play()
 
-            print("Now playing: " + ''.join(self.__music_list[music_rand]))
+            print("Now playing: " + "".join(self.__music_list[music_rand]))
