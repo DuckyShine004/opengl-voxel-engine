0ab04efd649cc78658d9daddafa06c8f16b9fb50
Gallon Zhou <123701457+DuckyShine004@users.noreply.github.com>
2023-11-10 22:18:43 +1300

    refactor: Refactored The Terrain Module

--- shaders/frag_shader.glsl
+++ shaders/frag_shader.glsl
@@ -76,5 +76,5 @@ void main() {
     FragColor = vec4(color, 1.0);
 
     // Debugging
-    //FragColor = vec4((fs_in.normal + 1.0) / 2.0, 1.0);
+    // FragColor = vec4((fs_in.normal + 1.0) / 2.0, 1.0);
 }
--- src/world/terrain.py
+++ src/world/terrain.py
@@ -4,11 +4,91 @@ It is not a manager itself because this is not a generic manager class.
 """
 from __future__ import annotations
 
+import glm
+import numpy
+
 from numba import njit
 
 from world.noise import get_noise_2d, get_noise_3d
 
-from constants.shape_constants import CHUNK_SIZE, CHUNK_HEIGHT, FREQUENCY, AMPLITUDE
+from constants.shape_constants import (
+    CHUNK_SIZE,
+    CHUNK_HEIGHT,
+    FREQUENCY,
+    AMPLITUDE,
+    OCTAVES,
+    EXPONENT,
+    PERSISTENCE,
+)
+
+
+@njit
+def _get_height(x: float, y: float) -> float:
+    """Returns the height map for the given x, y coordinates. The parameters
+    can be adjusted to get the desired height map, or terrain. Utilises the
+    performance boost from the njit decorator.
+
+    Args:
+        x (float): The x coordinate for noise sampling.
+        y (float): The y coordinate for noise sampling.
+
+    Returns:
+        float: The noise value.
+    """
+
+    noise_value = 0.0
+    amplitude_sum = 0.0
+
+    for i in range(OCTAVES):
+        amplitude = pow(2, i)
+        frequency = 1.0 / amplitude
+
+        dx = PERSISTENCE * frequency * x
+        dy = PERSISTENCE * frequency * y
+
+        dh = get_noise_2d(dx, dy) / 2.0 + 0.5
+
+        noise_value += amplitude * dh
+
+        amplitude_sum += amplitude
+
+    return int(pow(noise_value * 0.95, EXPONENT)) - (2 * amplitude_sum)
+
+
+@njit
+def _set_chunk() -> Tuple[numpy.ndarray, numpy.ndarray]:
+    """Sets the chunk for the given x, and z positions. If the chunk has
+    already been set, then we simply return nothing. Utilises the performance
+    boost from the njit decorator.
+
+    Returns:
+        Tuple[numpy.ndarray, numpy.ndarray]: Returns two values, the voxel position data,
+        and the voxel texture data.
+    """
+
+    voxel_data = []
+
+    for x in range(CHUNK_SIZE):
+        for z in range(CHUNK_SIZE):
+            dx = AMPLITUDE * (x / FREQUENCY)
+            dz = AMPLITUDE * (z / FREQUENCY)
+
+            y = _get_height(dx, dz)
+
+            voxel_data.append(((x, y, z), 0))
+
+            for d in range(1):
+                dy = y - d - 1
+
+                if d <= 5:
+                    voxel_data.append(((x, dy, z), 1))
+                else:
+                    voxel_data.append(((x, dy, z), 2))
+
+    position_data = numpy.array([voxel_datum[0] for voxel_datum in voxel_data], dtype="float32")
+    texture_data = numpy.array([voxel_datum[1] for voxel_datum in voxel_data], dtype="float32")
+
+    return position_data, texture_data
 
 
 class Terrain:
@@ -16,41 +96,21 @@ class Terrain:
     """The Terrain class will handle all things related to terrain."""
 
     @staticmethod
-    @njit
-    def get_height(x: float, y: float, exp: float = 1.7, persistence: float = 0.3) -> float:
+    def get_height(x: float, y: float) -> float:
         """Returns the height map for the given x, y coordinates. The
         parameters can be adjusted to get the desired height map, or terrain.
 
         Args:
             x (float): The x coordinate for noise sampling.
             y (float): The y coordinate for noise sampling.
-            exp (float, optional): The exponential value which exponentiates the noise value.
-            persistence (float, optional): How detailed we want the terrain to be.
 
         Returns:
             float: The noise value.
         """
 
-        noise_value = 0.0
-        amplitude_sum = 0.0
-
-        for i in range(4):
-            amplitude = pow(2, i)
-            frequency = 1.0 / amplitude
-
-            dx = persistence * frequency * x
-            dy = persistence * frequency * y
-
-            dh = get_noise_2d(dx, dy) / 2.0 + 0.5
-
-            noise_value += amplitude * dh
-
-            amplitude_sum += amplitude
-
-        return int(pow(noise_value * 0.95, exp)) - (2 * amplitude_sum)
+        return _get_height(x, y)
 
     @staticmethod
-    @njit
     def set_chunk() -> Tuple[numpy.ndarray, numpy.ndarray]:
         """Sets the chunk for the given x, and z positions. If the chunk has
         already been set, then we simply return nothing.
@@ -60,26 +120,4 @@ class Terrain:
             and the voxel texture data.
         """
 
-        voxel_data = []
-
-        for x in range(CHUNK_SIZE):
-            for z in range(CHUNK_SIZE):
-                dx = AMPLITUDE * (x / FREQUENCY)
-                dz = AMPLITUDE * (z / FREQUENCY)
-
-                y = Terrain.get_height(dx, dz)
-
-                translations.append((glm.vec3(x, y, z), 0))
-
-                for d in range(10):
-                    dy = y - d - 1
-
-                    if d <= 5:
-                        translations.append((glm.vec3(x, dy, z), 1))
-                    else:
-                        translations.append((glm.vec3(x, dy, z), 2))
-
-        position_data = numpy.array([voxel_data[0] for voxel_datum in voxel_data], dtype="float32")
-        texture_data = numpy.array([voxel_data[1] for voxel_datum in voxel_data], dtype="float32")
-
-        return position_data, texture_data
+        return _set_chunk()
