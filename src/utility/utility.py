9ce44300fdeddec2e468b26c3bf79f27b778132e
quincr <102488576+quincr@users.noreply.github.com>
2023-11-30 10:30:14 -0600

    Moved utility additions to a seperate branch

--- src/utility/utility.py
+++ src/utility/utility.py
@@ -25,3 +25,48 @@ class Utility:
         """
 
         return max(min(value, maximum), minimum)
+
+    @staticmethod
+    def lerp(value: float, minimum: float, maximum: float) -> float:
+        """Lerps the given value between (or in relation to) the two ranges: minimum and maximum.
+        
+        Args:
+            value (float): The value to be lerped.
+            minimum (float): The lower bound.
+            maximum (float): The upper bound.
+        
+        Returns:
+            float: The lerped value.
+        """
+
+        return minimum + (maximum - minimum) * value
+    
+    @staticmethod
+    def inverse_lerp(value: float, minimum: float, maximum: float) -> float:
+        """Finds the location of the value between (or in relation to) the two ranges: minimum and maximum.
+        
+        Args:
+            value (float): The value to be found.
+            minimum (float): The lower bound.
+            maximum (float): The upper bound.
+        
+        Returns:
+            float: The found value.
+        """
+
+        return (value - minimum) / (maximum - minimum)
+    
+    @staticmethod
+    def smoothstep(value : float, minimum: float, maximum: float) -> float:
+        """Smoothly lerps the givel value between (or in relation to) the two ranges: minimum and maximum.
+        
+        Args:
+            value (float): The value to be smoothstepped.
+            minimum (float): The lower bound.
+            maximum (float): The upper bound.
+        
+        Returns:
+            float: The smoothstepped value.
+        """
+
+        return Utility.lerp(value * value * (3 - 2 * value), minimum, maximum)
